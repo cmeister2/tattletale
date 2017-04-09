1b2d56bbae3eba013dd002064a4c647766282017
cmeister2 <cmeister2@hotmail.com>
2017-04-09 21:26:53 +0100

    Initial package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="tattletale",
+      version="0.1.0",
+      description="An internet reconnaissance tool",
+      url="http://github.com/cmeister2/tattletale",
+      author="cmeister2",
+      author_email="cmeister2@gmail.com",
+      license="MIT",
+      packages=["tattletale"],
+      zip_safe=False)
